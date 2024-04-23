eb1b3bcfb2a2e7383cec7cf2d4b16b70f848437c
buttertaii <buttertaii@MacBook-Air-khxng-buttertaii.local>
2024-04-23 22:11:51 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './App.css'
+import LoginSignup from './LoginSignup/LoginSignup'
+
+function App() {
+  return (
+    <div className="app">
+      <LoginSignup />
+    </div>
+  )
+}
+
+export default App
